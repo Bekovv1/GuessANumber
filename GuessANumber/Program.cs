bd14ad1477e81af924edca622e7a93d069e50c99
Bekovv1 <129979214+Bekovv1@users.noreply.github.com>
2023-04-29 23:32:58 +0300

    Add files via upload

---
+++ GuessANumber/Program.cs
@@ -0,0 +1,42 @@
+﻿Random Number = new Random();
+
+int PCNumber = Number.Next(1, 101);
+
+while (true)
+{
+    Console.WriteLine("Guess a number(1 - 100):");
+    string input = Console.ReadLine();
+    
+    bool isValid = int.TryParse(input, out int playerNum);
+    
+    if (isValid)
+    {
+        
+        if (playerNum == PCNumber)
+        {
+            Console.WriteLine("You guessed it!");
+            break;
+
+        }
+        
+        else if (playerNum > PCNumber)
+        
+        {
+            Console.WriteLine("Too High");
+        }
+        
+        else
+        
+        {
+            Console.WriteLine("Too Low");
+        
+        }
+
+
+    }
+    
+    else
+    {
+        Console.WriteLine("Invalid input");
+    }
+}
